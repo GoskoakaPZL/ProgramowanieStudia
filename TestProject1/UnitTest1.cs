0bb38e42731b6f240b26d82d775b5c7ac1bf42f0
GoskoakaPZL <pzlrozrabiaka@gmail.com>
2022-06-13 21:50:04 +0200

    Drugi Test Jednostkowy

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -24,6 +24,24 @@ namespace TestProject1
             Assert.AreNotEqual(x2, double.NaN);
 
         }
+
+        [TestClass]
+        public class Testjednostkowy2
+        {
+            [TestMethod]
+            public void Test1()
+            {
+                double x1, x2;
+                var a = 1;
+                var b = 2;
+                var c = 1;
+                zadanie4.obliczanie.rownaniekwadratowe(a, b, c, out x1, out x2);
+                Assert.AreEqual(x1, x2);
+
+
+
+            }
+        }
     }
 }
 
