1cfcee3b1542631085bd8e8a25de752d4e14c569
GoskoakaPZL <pzlrozrabiaka@gmail.com>
2022-06-13 21:48:17 +0200

    Pierwszy Test Jednostkowy

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -4,12 +4,26 @@ using static zadanie4.obliczanie;
 
 namespace TestProject1
 {
+
     [TestClass]
-    public class UnitTest1
+    public class Testjednostkowy1
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void Test1()
         {
+            zadanie4.obliczanie l = new zadanie4.obliczanie();
+
+            double x1, x2;
+            var a = 2;
+            var b = 5;
+            var c = 1;
+            zadanie4.obliczanie.rownaniekwadratowe(a, b, c, out x1, out x2);
+
+            Assert.AreNotEqual(x1, double.NaN);
+            Assert.AreNotEqual(x2, double.NaN);
+
         }
     }
 }
+
