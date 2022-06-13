e5f7b366ec1efcd0dfd48292080142d62097a49e
GoskoakaPZL <pzlrozrabiaka@gmail.com>
2022-06-13 21:47:07 +0200

    Testy Początki

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static zadanie4.obliczanie;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
